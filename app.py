3ce5565c677f2d5fcdbc6f2999b4cd458fe89f63
Candas39 <ricardocandas33@gmail.com>
2024-11-05 18:40:39 -0600

    Initial commit with first version of tracker INEGI data

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request, jsonify, render_template
+from utils.get_info import get_info_inegi
+from database.sqlite import insert_data, filter_data 
+import json
+
+
+
+app = Flask(__name__)
+
+    
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/tabla', methods=['GET', 'POST'])
+def index_tabla():
+    tipo_establecimiento = request.args.get("tipo_establecimiento")
+    coordenadas = request.args.get("coordenadas")
+    radio = request.args.get("radio")
+    
+    # Imprimir para depuración
+    print(tipo_establecimiento, radio, coordenadas)
+    
+    datos = get_info_inegi(tipo_establecimiento, coordenadas, radio)
+    
+    # Crear el JSON para la plantilla
+    json_data = {
+        "tipo_establecimiento": tipo_establecimiento,
+        "coordenadas": coordenadas,
+        "radio": radio
+    }
+    
+    json_data = json.dumps(json_data)  # Convertir a JSON para la plantilla
+    
+    # Si la solicitud pide JSON, responder con datos en formato JSON
+    if request.args.get("json"):
+        return jsonify(datos)
+    
+    # Renderizar la plantilla con los datos JSON
+    return render_template('table.html', json_data=json_data)
+
+@app.route('/buscar_establecimientos', methods=['POST'])
+def buscar_establecimientos():
+    data = request.get_json()
+    if not data or not all(k in data for k in ("tipo_establecimiento", "coordenadas", "radio")):
+        return jsonify({"error": "Parámetros incompletos"}), 400
+    
+    tipo_establecimiento = data.get("tipo_establecimiento")
+    coordenadas = data.get("coordenadas")
+    radio = data.get("radio")
+    resultado = get_info_inegi(tipo_establecimiento, coordenadas, radio)
+    return jsonify(resultado), 200
+
+
+@app.route('/insert_data', methods=['POST'])
+def insert_data_api():
+    data = request.get_json()
+    if not data or not all(k in data for k in ("tipo_establecimiento", "coordenadas", "radio")):
+        return jsonify({"error": "Parámetros incompletos"}), 400
+    
+    tipo_establecimiento = data.get("tipo_establecimiento")
+    coordenadas = data.get("coordenadas")
+    radio = data.get("radio")
+    resultado = get_info_inegi(tipo_establecimiento, coordenadas, radio)
+    return jsonify(insert_data(resultado, 'inegi_test.db', tipo_establecimiento)), 200
+
+
+@app.route('/filter_data', methods=['POST'])
+def search_data_api():
+    data = request.get_json()
+    if not data or "palabra_clave" not in data:
+        return jsonify({"error": "Parámetros incompletos"}), 400
+
+    palabra_clave = data.get("palabra_clave")
+    resultados = filter_data(palabra_clave)
+    return jsonify(resultados), 200
+    
+
+
+if __name__ == '__main__':
+    app.app_context().push()
+    app.run(debug=True, port=5000)
+ 
